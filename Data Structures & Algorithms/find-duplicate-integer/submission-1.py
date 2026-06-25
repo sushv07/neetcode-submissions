70efe077d3488385cd32ee425590dd9f56e29bf0
Sushmitha Vijayakumar <sushv07@gmail.com>
2026-06-24 18:26:45 -0700

    Add: find-duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        #O(n) TC and O(1) SC
+        #fast and slow pointers
+        #Floyd's Hare and Tortoise cycle detection
+        slow, fast = 0, 0
+
+        #detect meting point inside cycle
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+        #find the starting point of cycle (i.e the duplicate)
+        #he duplicate value is the first node (or value) 
+        #reached from index 0 where two different paths merge.
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
+            
+
+
+
+        
